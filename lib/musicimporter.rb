cc846d5deed256042c1f6b2187d6cdd6dcea64db
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-08 02:51:25 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,21 +1,17 @@
 class MusicImporter
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
   end
 
-  def path
-    @path
+  def files
+      @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
-  def files()
-    @files ||= Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
+  def import
+    files.each {|file| Song.create_from_filename(file) }
   end
 
-  def import()
-    files.each do |filename|
-      Song.create_from_filename(filename)
-    end
-  end
 
 end
