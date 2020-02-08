f2159957784ef6ac86aa94c3d0a47e41c6966f75
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-08 02:51:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,8 +1,8 @@
 class Genre
-
   extend Concerns::Findable
-
   attr_accessor :name
+  attr_reader :songs
+
   @@all = []
 
   def initialize(name)
@@ -23,27 +23,16 @@ class Genre
   end
 
   def self.create(name)
-    self.new(name).tap do |genre|
-      genre.save
-    end
+    genre = self.new(name)
+    genre.save
+    genre
   end
 
   def songs
     @songs
   end
 
-
-  def add_song(song)
-    song.genre = self unless song.genre == self
-    @songs << song unless @songs.include?(song)
-  end
-
-
   def artists
-    artists = @songs.collect do |song|
-      song.artist
-    end
-    artists.uniq
+    self.songs.collect {|s| s.artist}.uniq
   end
-
 end
